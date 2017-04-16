b8ab5693b62ee84ead5f4435ffaa056b97081c2d
Leland Batey <lelandbatey@gmail.com>
2017-04-16 12:34:02 -0700

    Add script for generating deep zoom

--- make_frames.sh
+++ make_frames.sh
@@ -2,12 +2,19 @@
 
 mkdir -p frames
 
-for i in $(seq -w  0 0.002 16)
+RESOLUTION=1920
+THREADS=6
+
+# Some real good coordinates:
+# -x -0.701000092002025 -y 0.351000000999792
+
+#for i in $(seq -w  0 0.002 16)
+for i in $(seq -w  0 0.01 51)
 do
 	PGMPATH="./frames/img$i.pgm"
 	PNGPATH="./frames/img$i.png"
-	echo "./target/release/mandelbrot -r 1920 -z \"$i\" > $PGMPATH"
-	./target/release/mandelbrot -r 1920 -z "$i" > "$PGMPATH"
+	echo "./target/release/mandelbrot --max_iters 400 -r $RESOLUTION -t $THREADS -x -0.701000092002025 -y 0.351000000999792 -z \"$i\" > $PGMPATH"
+	./target/release/mandelbrot --max_iters 400 -r "$RESOLUTION" -t "$THREADS" -x -0.701000092002025 -y 0.351000000999792 -z "$i" > "$PGMPATH"
 	convert "$PGMPATH" "$PNGPATH"
 	rm "$PGMPATH"
 done
